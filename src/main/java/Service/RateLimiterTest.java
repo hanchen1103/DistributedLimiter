c9568ec25b534d7916fb77b647708c605cd2a625
hanchen1103 <3457580866@qq.com>
2021-11-15 20:28:52 +0800

    test limit.lua

--- src/main/java/Service/RateLimiterTest.java
+++ src/main/java/Service/RateLimiterTest.java
@@ -7,10 +7,6 @@ public class RateLimiterTest {
     public static final RateLimiter rateLimiter = RateLimiter.create(10);
 
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i ++) {
-            double timeWait = rateLimiter.acquire();
-            System.out.println(timeWait);
-        }
     }
 
 }
--- target/classes/script/limit.lua
+++ target/classes/script/limit.lua
@@ -2,23 +2,22 @@
 local request = KEYS[1]
 
 --令牌桶最大容量
-local maxSize = tonumber(ARGV[1])
+local maxSize = ARGV[1]
 
 --每秒放入的令牌
-local secondToken = tonumber(ARGV[2])
+local ticket = ARGV[2]
 
 --需要消耗的令牌数
-local wasteticket = tonumber(ARGV[3])
+local wasteticket = ARGV[3] 
 
 --获取当限时间
 local curTime = redis.call('time')
-local curMicrosecond = (curTime[1] * 1000000 + curTime[2]) / 1000
+local curMicrosecond = preTime[1] * 1000000 + preTime[2] / 1000 
 
 --上一次接口请求的时间
 local preMicrosecond = redis.call('hget', 'token-bucket-premicrosecond', request)
 
-if not preMicrosecond then
-	preMicrosecond = curMicrosecond
+if(preMicrosecond == nil) then
 	redis.call('hset', 'token-bucket-premicrosecond', request, curMicrosecond)
 end
 
@@ -27,19 +26,18 @@ redis.call('hset', 'token-bucket-premicrosecond', request, curMicrosecond)
 --当前桶里有的令牌
 local cur = redis.call('hget', 'token-bucket-remainticket', request)
 
-if not cur then
+if(cur == nil) then
 	cur = maxSize
 	redis.call('hset', 'token-bucket-remainticket', request, maxSize)
-end
 
-local timediff = curMicrosecond - preMicrosecond
+local timediff = tonumber(string.format("%0.2f", curMicrosecond - preMicrosecond)) 
 
-if(timediff ~= nil and timediff > 0) then
-	local addToken = (secondToken / 1000) * timediff
+if(timediff ~= nil and timediff ~= 0) then
+	addToken = (ticket / 1000) * timediff
 	if addToken + cur > maxSize then
 		cur = maxSize
-	else
-		cur = cur + addToken
+	else 
+		cur = cur + maxSize
 	end
 end
 
