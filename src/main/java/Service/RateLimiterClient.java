d1404b2928884ea4b049b46ee8c6a3809c3079c1
hanchen1103 <3457580866@qq.com>
2021-11-16 19:49:31 +0800

    test limit

---
+++ src/main/java/Service/RateLimiterClient.java
@@ -0,0 +1,13 @@
+package Service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class RateLimiterClient {
+
+
+}
--- target/classes/script/limit.lua
+++ target/classes/script/limit.lua
@@ -2,22 +2,23 @@
 local request = KEYS[1]
 
 --令牌桶最大容量
-local maxSize = ARGV[1]
+local maxSize = tonumber(ARGV[1])
 
 --每秒放入的令牌
-local ticket = ARGV[2]
+local secondToken = tonumber(ARGV[2])
 
 --需要消耗的令牌数
-local wasteticket = ARGV[3] 
+local wasteticket = tonumber(ARGV[3])
 
 --获取当限时间
 local curTime = redis.call('time')
-local curMicrosecond = preTime[1] * 1000000 + preTime[2] / 1000 
+local curMicrosecond = (curTime[1] * 1000000 + curTime[2]) / 1000
 
 --上一次接口请求的时间
 local preMicrosecond = redis.call('hget', 'token-bucket-premicrosecond', request)
 
-if(preMicrosecond == nil) then
+if not preMicrosecond then
+	preMicrosecond = curMicrosecond
 	redis.call('hset', 'token-bucket-premicrosecond', request, curMicrosecond)
 end
 
@@ -26,18 +27,19 @@ redis.call('hset', 'token-bucket-premicrosecond', request, curMicrosecond)
 --当前桶里有的令牌
 local cur = redis.call('hget', 'token-bucket-remainticket', request)
 
-if(cur == nil) then
+if not cur then
 	cur = maxSize
 	redis.call('hset', 'token-bucket-remainticket', request, maxSize)
+end
 
-local timediff = tonumber(string.format("%0.2f", curMicrosecond - preMicrosecond)) 
+local timediff = curMicrosecond - preMicrosecond
 
-if(timediff ~= nil and timediff ~= 0) then
-	addToken = (ticket / 1000) * timediff
+if(timediff ~= nil and timediff > 0) then
+	local addToken = (secondToken / 1000) * timediff
 	if addToken + cur > maxSize then
 		cur = maxSize
-	else 
-		cur = cur + maxSize
+	else
+		cur = cur + addToken
 	end
 end
 
