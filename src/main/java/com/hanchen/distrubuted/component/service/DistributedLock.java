4a46227d26a8a72f2dbf9c9a8a81b7fba67d4244
hanchen1103 <3457580866@qq.com>
2021-11-20 02:36:56 +0800

    add zk-dependency

---
+++ src/main/java/com/hanchen/distrubuted/component/service/DistributedLock.java
@@ -0,0 +1,20 @@
+package com.hanchen.distrubuted.component.service;
+
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import util.ScriptUtil;
+
+public class DistributedLock {
+
+    private JedisConnectionFactory jedisConnectionFactory;
+
+    private String script;
+
+    private String keyRequest;
+
+    private String valueRequest;
+
+    private void loadScript() {
+        script =  ScriptUtil.loadScript("script/lock.lua");
+    }
+
+}
